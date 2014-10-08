c943c248ad5cc163ec9e450e92f8093a0ed16d00
Sam Sarette <samuelsarette@linux.com>
2014-10-08 14:10:01 -0400

    Re-added old tests

--- test.sh
+++ test.sh
@@ -4,19 +4,19 @@ relaunchIfNotVisible
 
 APP_NAME="Test Script"
 
-# ACTIVITY="Salutations"
-# messagebox "Hello World";
-#
-# ACTIVITY="Inquiry"
-# yesno "Are you well?";
-# ANSWER=$?
-#
-# ACTIVITY="Response"
-# if [ $ANSWER -eq 0 ]; then
-#     messagebox "Good to hear."
-# else
-#     messagebox "Sorry to hear that."
-# fi
+ACTIVITY="Salutations"
+messagebox "Hello World";
+
+ACTIVITY="Inquiry"
+yesno "Are you well?";
+ANSWER=$?
+
+ACTIVITY="Response"
+if [ $ANSWER -eq 0 ]; then
+    messagebox "Good to hear."
+else
+    messagebox "Sorry to hear that."
+fi
 
 ACTIVITY="Name"
 ANSWER=$(inputbox "What's your name?" " ")
